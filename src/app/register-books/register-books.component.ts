75e76dee587f18790b30ffc18c21e72827a3cbaf
samuelmsilva2v <samuelmsilva@outlook.com.br>
2025-01-06 17:33:30 -0300

    feat: register book working

--- src/app/register-books/register-books.component.ts
+++ src/app/register-books/register-books.component.ts
@@ -1,4 +1,5 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
@@ -7,10 +8,18 @@ import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } 
   standalone: true,
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './register-books.component.html',
-  styleUrl: './register-books.component.css'
+  styleUrls: ['./register-books.component.css'],
+  providers: [DatePipe]  // Adicionado o DatePipe aqui
 })
 
 export class RegisterBooksComponent {
+
+  message: string = '';
+
+  constructor(
+    private httpClient: HttpClient,
+    private datePipe: DatePipe
+  ) {}
 
   formulario = new FormGroup({
     title : new FormControl('', [Validators.required]),
@@ -18,10 +27,25 @@ export class RegisterBooksComponent {
     genre : new FormControl('', [Validators.required]),
     publicationDate : new FormControl('', [Validators.required]),
     publisher : new FormControl('', [Validators.required]),
-    collection : new FormControl(''),
-  })
+    collection : new FormControl('')
+  });
 
   registerBook() {
-    console.log(this.formulario.value);
+    // Formatar a data antes de enviar
+    const formData = this.formulario.value;
+    formData.publicationDate = this.datePipe.transform(formData.publicationDate, 'dd/MM/yyyy');
+
+    // Enviar os dados formatados
+    this.httpClient.post('http://localhost:8080/api/books', formData, { responseType: 'text' }).subscribe({
+      next: (response) => {
+        this.message = response;
+        this.formulario.reset(); // Resetar o formulário após o sucesso
+      },
+      error: (error) => {
+        console.error('Erro ao registrar livro:', error);
+        this.message = 'Erro ao registrar livro. Por favor, tente novamente.';
+      }
+    });
   }
 }
+
