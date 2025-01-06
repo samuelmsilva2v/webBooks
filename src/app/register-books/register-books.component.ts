11da7a5bfedd836fba53c382f66c1f3963cc4dfc
samuelmsilva2v <samuelmsilva@outlook.com.br>
2025-01-06 16:27:16 -0300

    feat: register book form control

--- src/app/register-books/register-books.component.ts
+++ src/app/register-books/register-books.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-books',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './register-books.component.html',
   styleUrl: './register-books.component.css'
 })
+
 export class RegisterBooksComponent {
 
+  formulario = new FormGroup({
+    title : new FormControl('', [Validators.required]),
+    author : new FormControl('', [Validators.required]),
+    genre : new FormControl('', [Validators.required]),
+    publicationDate : new FormControl('', [Validators.required]),
+    publisher : new FormControl('', [Validators.required]),
+    collection : new FormControl(''),
+  })
+
+  registerBook() {
+    console.log(this.formulario.value);
+  }
 }
