cb27eae18d1d7469e19e97cb72f14b2267571a3f
samuelmsilva2v <samuelmsilva@outlook.com.br>
2025-01-07 17:54:52 -0300

    feat: delete book button

--- src/app/consult-books/consult-books.component.ts
+++ src/app/consult-books/consult-books.component.ts
@@ -13,30 +13,46 @@ import { NgxPaginationModule } from 'ngx-pagination';
 
 export class ConsultBooksComponent implements OnInit {
 
-  books: any[] = []; 
+  books: any[] = [];
+  message: string = '';
   errorMessage: string = '';
   paginator: number = 1;
 
   constructor(private httpClient: HttpClient) { }
 
   ngOnInit(): void {
-    this.fetchBooks(); 
+    this.fetchBooks();
   }
 
-  handlePageChange(event: any) {
-    this.paginator = event;
+  deleteBook(id: string) {
+
+    if (confirm('Do you really want to delete the selected book ?')) {
+      this.httpClient.delete('http://localhost:8080/api/books/' + id,
+      { responseType: 'text' })
+        .subscribe({
+          next: (resposta) => {
+            this.message = resposta; 
+            this.ngOnInit(); 
+          }
+        });
   }
+}
 
 
-  fetchBooks(): void {
-    this.httpClient.get<any[]>('http://localhost:8080/api/books').subscribe({
-      next: (response) => {
-        this.books = response; 
-      },
-      error: (error) => {
-        console.error('Erro ao buscar livros:', error); 
-        this.errorMessage = 'Erro ao carregar os livros. Tente novamente mais tarde.';
-      },
-    });
-  }
+handlePageChange(event: any) {
+  this.paginator = event;
+}
+
+
+fetchBooks(): void {
+  this.httpClient.get<any[]>('http://localhost:8080/api/books').subscribe({
+    next: (response) => {
+      this.books = response;
+    },
+    error: (error) => {
+      console.error('Erro ao buscar livros:', error);
+      this.errorMessage = 'Erro ao carregar os livros. Tente novamente mais tarde.';
+    },
+  });
+}
 }
