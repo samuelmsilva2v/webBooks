efbbd9a6c0f1a549d3615292de2c3b189d3dc22f
samuelmsilva2v <samuelmsilva@outlook.com.br>
2025-01-07 17:43:23 -0300

    feat: consult books component

--- src/app/consult-books/consult-books.component.ts
+++ src/app/consult-books/consult-books.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-consult-books',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgxPaginationModule],
   templateUrl: './consult-books.component.html',
-  styleUrl: './consult-books.component.css'
+  styleUrls: ['./consult-books.component.css'],
 })
-export class ConsultBooksComponent {
 
+export class ConsultBooksComponent implements OnInit {
+
+  books: any[] = []; 
+  errorMessage: string = '';
+  paginator: number = 1;
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit(): void {
+    this.fetchBooks(); 
+  }
+
+  handlePageChange(event: any) {
+    this.paginator = event;
+  }
+
+
+  fetchBooks(): void {
+    this.httpClient.get<any[]>('http://localhost:8080/api/books').subscribe({
+      next: (response) => {
+        this.books = response; 
+      },
+      error: (error) => {
+        console.error('Erro ao buscar livros:', error); 
+        this.errorMessage = 'Erro ao carregar os livros. Tente novamente mais tarde.';
+      },
+    });
+  }
 }
--- src/app/register-books/register-books.component.ts
+++ src/app/register-books/register-books.component.ts
@@ -19,31 +19,30 @@ export class RegisterBooksComponent {
   constructor(
     private httpClient: HttpClient,
     private datePipe: DatePipe
-  ) {}
+  ) { }
 
   formulario = new FormGroup({
-    title : new FormControl('', [Validators.required]),
-    author : new FormControl('', [Validators.required]),
-    genre : new FormControl('', [Validators.required]),
-    publicationDate : new FormControl('', [Validators.required]),
-    publisher : new FormControl('', [Validators.required]),
-    collection : new FormControl('')
+    title: new FormControl('', [Validators.required]),
+    author: new FormControl('', [Validators.required]),
+    genre: new FormControl('', [Validators.required]),
+    publicationDate: new FormControl('', [Validators.required]),
+    publisher: new FormControl('', [Validators.required]),
+    collection: new FormControl('')
   });
 
   registerBook() {
-    // Formatar a data antes de enviar
+    
     const formData = this.formulario.value;
     formData.publicationDate = this.datePipe.transform(formData.publicationDate, 'dd/MM/yyyy');
 
-    // Enviar os dados formatados
     this.httpClient.post('http://localhost:8080/api/books', formData, { responseType: 'text' }).subscribe({
-      next: (response) => {
-        this.message = response;
-        this.formulario.reset(); // Resetar o formulário após o sucesso
+      next: () => {  // Ignore o conteúdo da resposta, já que não precisamos usá-lo
+        this.message = 'Livro registrado com sucesso!'; // Define a mensagem de sucesso
+        this.formulario.reset(); // Reseta o formulário após o sucesso
       },
       error: (error) => {
         console.error('Erro ao registrar livro:', error);
-        this.message = 'Erro ao registrar livro. Por favor, tente novamente.';
+        this.message = 'Erro ao registrar livro. Por favor, tente novamente.'; // Mensagem de erro
       }
     });
   }
